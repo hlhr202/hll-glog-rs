aa52ee39b153e85119bbdb994daf875a53d5d0fb
hlhr202 <hlhr202@163.com>
2023-08-05 19:59:28 +0800

    feat: impl http reader

---
+++ examples/http.rs
@@ -0,0 +1,27 @@
+use std::io::BufReader;
+
+use axum::{extract::Multipart, routing::post, Router};
+use glog_rust::log_reader;
+
+async fn upload(mut multipart: Multipart) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let pri_key = std::env::var("PRI_KEY").unwrap();
+        if name == "file" {
+            let data = field.bytes().await.unwrap();
+            let reader = BufReader::new(data.as_ref());
+            log_reader::read(reader, &pri_key, |content| println!("{}", content)).unwrap();
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenvy::from_path(".env.local").unwrap();
+    let app = Router::new().route("/", post(upload));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
